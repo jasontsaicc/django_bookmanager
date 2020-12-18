1de0718125309acaabfa8d0f1ae7f2136573c8b1
jasontsaicc <jason.tsaicc@gmail.com>
2020-12-19 00:30:18 +0800

    1.定義模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django import models
 
 # Create your models here.
+
+# """"
+# # 1.定義模型
+# # 2.模型遷移
+# # 3.操作數據庫
+#
+# 要知道
+# 1.在那裡定義模型 (model)
+# 2.模型繼承自誰
+# 3.ORM對應關係
+# 表 》類
+# 字段 》屬性
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    #性別
+    gender = models.BooleanField()
+    # 外鍵
+    book = models.ForeignKey(BookInfo)
